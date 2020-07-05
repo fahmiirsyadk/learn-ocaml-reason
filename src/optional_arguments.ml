1dbf3e4420ed49dc4178721f2f6a1893b73c9ed2
fahmi irsyad khairi <fahmiirsyad10@gmail.com>
2020-07-06 06:42:48 +0700

    update optional arguments

--- src/optional_arguments.ml
+++ src/optional_arguments.ml
@@ -1,6 +1,9 @@
-let concat ?sep x y =
-  (fun x y -> fun (z) -> match z with None -> x ^ y | Some z -> x ^ z ^ y) x y sep
+let concat ?sep x y = match sep with None -> x ^ y | Some z -> x ^ z ^ y
+
+let uppercase_concat ?(sep="") a b = concat ~sep (String.uppercase_ascii a) b
 
 let () = 
   concat "hello" "world" |> Js.log;
-  concat ~sep:"--" "hello" "world" |> Js.log
+  concat ~sep:"--" "hello" "world" |> Js.log;
+  uppercase_concat "hello" "world" |> Js.log;
+  uppercase_concat "hello" "world" ~sep:":" |> Js.log
--- src/optional_arguments_reason.re
+++ src/optional_arguments_reason.re
@@ -11,5 +11,10 @@ let concat = (~sep=?, x, y) =>
     sep,
   );
 
+let uppercase_concat = (~sep="", a, b) =>
+  concat(~sep, String.uppercase_ascii(a), b);
+
 let () = concat("hello", "world") |> Js.log;
 concat(~sep="~~", "hello", "world") |> Js.log;
+uppercase_concat("hello", "world") |> Js.log;
+uppercase_concat("hello", "world", ~sep=":") |> Js.log;
